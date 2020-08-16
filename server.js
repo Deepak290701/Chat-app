118a087828858276861642fd375c6f901530f033
Deepak290701 <deepaksharma290700@gmail.com>
2020-08-17 02:42:06 +0530

    addes remaing functionalities like emiting mssg using socket.io and receiving it

--- server.js
+++ server.js
@@ -21,4 +21,7 @@ http.listen(PORT, ()=>{
 const io = require('socket.io')(http)
 io.on('connection',(socket)=>{
     console.log('connected')
+    socket.on('message',(msg) => {
+        socket.broadcast.emit('message',msg);
+    })
 })
